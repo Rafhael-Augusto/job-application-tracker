0f037e36708618b2954d07bfdf03f82731111908
Rafhael <rafhaelaugustodev@gmail.com>
2026-05-02 09:53:08 -0300

    refactor: remove images component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { Features } from "@/components/features/features";
 import { Hero } from "@/components/hero/hero";
-import { ImageTabs } from "@/components/imageTabs/imageTabs";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <ImageTabs />
       <Features />
     </main>
   );
